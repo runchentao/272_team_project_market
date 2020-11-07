6618683d23fbacc90331e6d7dcf8037b9c84f8ba
Reneechen108 <reneechen108@gmail.com>
2020-11-07 00:02:55 -0800

    adjust heater

--- header.php
+++ header.php
@@ -24,6 +24,9 @@
     <li class="nav-item">
       <a class="nav-link" href="#">Link</a>
     </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
     <form class="form-inline my-2 my-md-0">
       <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
