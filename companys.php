b83319acbecf9d003851b13bcc986c52b717ad1e
runchentao <runchentao@gmail.com>
2020-11-16 11:39:20 -0800

    added runchen company link and card

--- companys.php
+++ companys.php
@@ -75,17 +75,24 @@
 
     <div class="card mb-4 shadow-sm">
         <div class="card-header">
-            <h4 class="my-0 font-weight-normal">Enterprise 3</h4>
+            <h4 class="my-0 font-weight-normal">Restaurant & Bar</h4>
         </div>
         <div class="card-body">
-            <h1 class="card-title pricing-card-title">$3 <small class="text-muted">/ mo</small></h1>
+            <h1 class="card-title pricing-card-title">JiuBar></h1>
             <ul class="list-unstyled mt-3 mb-4">
-                <li>30 users included</li>
-                <li>15 GB of storage</li>
-                <li>Phone and email support</li>
-                <li>Help center access</li>
+                <li>Developed by: <b>Runchen Tao</b></li>
+                <li>Dine-in & Takeout</li>
+                <li>San Jose's best bar</li>
             </ul>
-            <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
+            <?php if(isset($_SESSION['user'])):?>
+            <form action="http://haoyangliu.com/272CompanyDemo/" method="POST">
+                <input type='hidden' name='userId' value='<?php echo $_SESSION['user'][0]?>'>
+                <input type="submit" name="market-redirect" value="Go to Webify">
+            </form>
+            <?php else: ?>
+            <span class="hiddenURL">https://runchen-tao.herokuapp.com/</span>
+            <button class="btn btn-primary nonlog-cmpy-visit">Go to JiuBar</button>
+            <?php endif; ?>
         </div>
     </div>
     <div class="card mb-4 shadow-sm">
