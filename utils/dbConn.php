14fb7e1bb61c84dafc31ff3188d5300a75ff663f
runchentao <runchentao@gmail.com>
2020-11-17 14:41:04 -0800

    test new db

--- utils/dbConn.php
+++ utils/dbConn.php
@@ -1,9 +1,9 @@
 <?php 
     global $mysqli;
-    $host = "us-cdbr-east-02.cleardb.com";
-    $db_username = "b0d767ef5f5387";
-    $db_password = "ac3d97ea";
-    $db_name = "heroku_f56e864c8482ef5";
+    $host = "blonze2d5mrbmcgf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $db_username = "hprl9qbvszejfo36";
+    $db_password = "vqc6ayrzx8ibuzb7";
+    $db_name = "zjcag0s2vznyd8it";
 
     //Create connection
     $mysqli = mysqli_connect($host, $db_username, $db_password, $db_name);
