15f4636b250b6dc535493a9521f97a792cc1bf74
runchentao <runchentao@gmail.com>
2020-12-07 17:35:21 -0800

    test

--- signin.php
+++ signin.php
@@ -33,6 +33,15 @@
 
 <script>
 function onSignIn(googleUser) {
+    if (auth2.isSignedIn.get()) {
+        var profile = auth2.currentUser.get().getBasicProfile();
+        console.log('ID: ' + profile.getId());
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+    }
     var profile = googleUser.getBasicProfile();
     var id_token = googleUser.getAuthResponse().id_token;
     var first_name = profile.getGivenName());
