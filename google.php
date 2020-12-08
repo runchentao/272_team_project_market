034972ea61b0cdb6d7bf3227249d89480232f6e3
runchentao <runchentao@gmail.com>
2020-12-08 12:11:08 -0800

    google auth test

--- google.php
+++ google.php
@@ -3,6 +3,11 @@
 <div class="g-signin2" data-onsuccess="onSignIn"></div>
 <script>
 function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     window.location.replace("https://market-place-272.herokuapp.com/googleSuccess.php");
 }
 </script>
--- googleSuccess.php
+++ googleSuccess.php
@@ -1,5 +1,4 @@
 <?php 
     session_start();
     $_SESSION['loggedin'] = true;
-    header("location: home.php");
 ?>
