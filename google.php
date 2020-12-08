1ce0386e934ac063faff129c117cfa074a54cb96
runchentao <runchentao@gmail.com>
2020-12-08 12:14:34 -0800

    google auth test

--- google.php
+++ google.php
@@ -1,13 +1,4 @@
 <script src="https://apis.google.com/js/platform.js" async defer></script>
 <meta name="google-signin-client_id" content="343939675754-mito5glsefguavihrbarr4r7td7fko24.apps.googleusercontent.com">
-<div class="g-signin2" data-onsuccess="onSignIn"></div>
-<script>
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    window.location.replace("https://market-place-272.herokuapp.com/googleSuccess.php");
-}
-</script>
+<div class="g-signin2"></div>
+<a href="https://market-place-272.herokuapp.com/googleSuccess.php">Back to Home Page</a>
