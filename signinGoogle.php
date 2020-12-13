15b052426a4dc6e1595ab36535ecd19af63b942e
runchentao <runchentao@gmail.com>
2020-12-12 20:16:35 -0800

    testing google auth

--- signinGoogle.php
+++ signinGoogle.php
@@ -1,11 +1,8 @@
 <?php
-    require_once('utils/dbConn.php');
     session_start();
     
     $_SESSION['googleLoggedin'] = true;
     $_SESSION['loggedin'] = true;
     
     header("location: home.php");
-
-    $conn->close();
 ?>
